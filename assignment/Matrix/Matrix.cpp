5f5a31d284a957447ce5c7703542335e041e0dc6
Cole Schneider <colekschneider@gmail.com>
2017-11-02 22:36:11 -0700

    working

--- assignment/Matrix/Matrix.cpp
+++ assignment/Matrix/Matrix.cpp
@@ -1,77 +1,77 @@
+//Matrix.cpp
+
 #include "Matrix.h"
-#include <cassert>
-//only do int rows=1 for class header
-Matrix::Matrix(int rows, int cols) {
-    //creats 0 rows x cols matrix filled with zeroes
-    _rows = rows;
-    _cols = cols;
-    for (int i = 0; i <rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            _body[i][j] = 0.0;
-        }
+
+Matrix::Matrix(int rows, int columns){
+  //creates and fill a matirx with all zeros
+  _rows = rows;
+  _columns = columns;
+  for (int i = 0; i < rows; i++){
+    for (int j = 0; j < columns; j++){
+      _body[i][j] = 0.0;
     }
+  }
 }
 
-int Matrix::rows() const {
-    return _rows;
-}
-
-int Matrix::cols() const {
-    return _cols;
-}
-
-//returns element at rows cols
-double Matrix::at(int i, int j) const {
-    assert(i >= 0 && j >= 0 && i < _rows && j < _cols);
-    return _body[i][j];
-}
-
-void Matrix::set(int i, int j, double val) {
-    // m.set(2, 3, 4.5);
-    assert(i >= 0 && j >= 0 && i < _rows && j < _cols);
-    _body[i][j] = val;
-}
-
-void Matrix::populate() {
-    for (int i = 0; i < rows(); i++) {
-        for (int j = 0; j < cols(); j++) {
-            cin >> _body[i][j];
-        }
+void Matrix::populate(){
+  for (int i = 0; i < rows(); i++){
+    for (int j = 0; j < columns(); j++){
+      cin >> _body[i][j];
     }
+  }
 }
 
-Matrix Matrix::transpose() const {
-    Matrix t(cols(), rows());
-    for (int i = 0; i < rows(); i++) {
-        for (int j = 0; j < cols(); j++) {
-            t.set(j, i, at(i, j)); // at(i, j) does same as _body[i][j]
-        }
-    }
-    return t;
+int Matrix::rows() const{
+  return _rows;
 }
 
-Matrix Matrix::identity(int n) {
-    Matrix id(n, n);
-    assert(n >= 0);
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j <n; j++) {
-            if (i == j) {
-                id.set(i, j, 1.0);
-            } else {
-                id.set(i, j, 0.0);
-            }
-        }
-    }
-    return id;
+int Matrix::columns() const{
+  return _columns;
 }
 
-ostream &operator <<(ostream &os, const Matrix &rhs) {
-    for (int i = 0; i <rhs.rows(); i++) {
-        for (int j = 0; j < rhs.cols(); i++) {
-            os.width(12);
-            os << rhs.at(i, j);
-        }
-        os << endl;
+double Matrix::at(int i, int j) const{
+  assert(i >= 0 && j >= 0 && i < _rows && j < _columns);
+  return _body[i][j];
+}
+
+void Matrix::set(int i, int j, double val){
+  // m.set(2, 3, 4.5);
+  assert(i >= 0 && j >= 0 && i < _rows && j < _columns);
+  _body[i][j] = val;
+}
+
+Matrix Matrix::transpose() const{
+  Matrix t(columns(), rows());
+  for (int i = 0; i < rows(); i++){
+    for (int j = 0; j < columns(); j++){
+      t.set(j, i, at(i, j));
     }
-    return os;
+  }
+  return t;
+}
+
+Matrix Matrix::identity(int n){
+  assert(n >= 0);
+  Matrix id(n, n);
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < n; j++){
+      if( i == j){
+        id.set(i, j, 1.0);
+      } else {
+        id.set(i, j, 0.0);
+      }
+    }
+  }
+  return id;
+}
+
+ostream &operator <<(ostream &os, const Matrix &rhs){
+  for (int i = 0; i < rhs.rows(); i++){
+    for (int j = 0; j < rhs.columns();j++){
+      os.width(12);
+      os << rhs.at(i, j);
+    }
+    os << endl;
+  }
+  return os;
 }
--- assignment/Matrix/Matrix.h
+++ assignment/Matrix/Matrix.h
@@ -1,58 +1,36 @@
+//Matrix.h
 
-#ifndef MATRIX_H
 #define MATRIX_H
 
-#include<cassert>
 #include <iostream>
+#include <cassert>
 using namespace std;
 
 const int SIZE = 8;
 
-class Matrix {
+class Matrix{
 private:
-    double _body[SIZE][SIZE]; //creates a 2-d array with 64 elements row x column
-    int _rows;
-    int _cols;
-
+  double _body[SIZE][SIZE]; //creates a 2D matrix with 64 elements
+  int _rows;
+  int _columns;
 public:
-    //Matrix m(3, 4) makes a new matrix 3x4 and matrix n; makes 1x1. Matrix p(5)
-    Matrix(int rows=1, int cols=1); //serves as the default constructor
-    void populate(); //fill matrix with user input
-    //accessor methods
-    int rows() const;
-    int cols() const;
-    double at(int i, int j) const; //returns _body[i][j]
-    void set(int i, int j, double val);
-    Matrix transpose() const; //flips rows cols 2x3 => 3x2  
-    //static - this means do it 'once'. not like a method where MAtrix m would inherit all the methods defind in clas
-    static Matrix identity(int n); // return an n x n identity Matrix
-
+  Matrix(int rows = 1, int columns = 1);
+  void populate(); //fill matrix with user input
+  int rows() const;
+  int columns() const;
+  double at(int i, int j) const; //returns body[i][j]
+  void set(int i, int j, double val);
+  Matrix transpose() const;
+  static Matrix identity(int n); // returns a nxn identity matrix
 };
-/*
-Matrix operator +(const Matrix &lhs, const Matrix &rhs);
-//binary subtraction
-Matrix operator -(const Matrix &lhs, const Matrix &rhs);
-//negation unary subtraction
-Matrix operator -(const Matrix &rhs);
-//Matrix multipication
-Matrix operator *(const Matrix &lhs, const Matrix &rhs);
-//Scalar multipication
-Matrix operator *(double lhs, const Matrix &rhs);
-Matrix operator *(const Matrix &lhs, double rhs); //Scalar multipication
-//  2x3 * 3x2 == size of 2x2
-*/
+
 ostream &operator <<(ostream &os, const Matrix &rhs);
 
-//m + n
-//m - n
-//-m
-//m * n
-// 
-
+// Matrix operator +(const Matrix &lhs, const Matrix &rhs);
+// Matrix operator -(const Matrix &lhs, const Matrix &rhs);
+// Matrix operator -(const Matrix &rhs);
+// Matrix operator *(const Matrix &lhs, const Matrix &rhs); //matrix multiplication
+// Matrix operator *(double lhs, const Matrix &rhs); //scalar multiplication
+// Matrix operator *(const Matrix &lhs, double rhs); //scalar multiplication
 
 //syntax of static method: Matrix.identity(3);
-
-
-
-
-#endif
