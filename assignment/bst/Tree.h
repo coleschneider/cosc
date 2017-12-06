8aa4a52d2e0cdbf88aa32dd359e13df453a5d776
Cole Schneider <colekschneider@gmail.com>
2017-12-06 00:03:40 -0800

    header for bst

--- assignment/bst/Tree.h
+++ assignment/bst/Tree.h
@@ -1,39 +1,43 @@
 #ifndef TREE_H
 #define TREE_H
 
-#include <iostream>
-#include <cassert>
-using namespace std;
-
 
 class Node {
     friend class Tree;
+
 private:
-    int _val;
     Node *_left;
+    int _val;
     Node *_right;
+
 public:
-    Node(int val);
+    Node(int value);
     ~Node();
+    void inOrder() const;
+    void preOrder() const;
+    void postOrder() const;
     int size() const;
-    void inOrder() const; //works together with Tree->inOrder()
+    int numLeaves() const;
+    int numInternals() const;
+    int height() const;
 };
 
 class Tree {
 
 private:
     Node *_root;
+
 public:
     Tree();
+    Tree(const Tree &tree);
     ~Tree();
-    void insert(int val);
+    void insert(int value);
     void inOrder() const;
+    void preOrder() const;
+    void postOrder() const;
     int size() const;
-    
+    int numLeaves() const;
+    int numInternals() const;
+    int height() const;
+    bool contains(int value) const;
 };
-
-
-
-
-
-#endif
